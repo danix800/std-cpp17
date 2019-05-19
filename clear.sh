a8267928becf22c2b0655c76e5d93356692814aa
Ding Fei <fei.ding@ustchcs.com>
2019-05-20 07:02:33 +0800

    ch6 sections added

--- clear.sh
+++ clear.sh
@@ -2,4 +2,4 @@ find . -name "*.aux" -exec rm -v {} \;
 find . -name "*.log" -exec rm -v {} \;
 find . -name "*.out" -exec rm -v {} \;
 find . -name "*.pdf" -exec rm -v {} \;
-rm -v *.toc
+rm -v *.toc *.fdb_latexmk *.fls *.synctex.gz
